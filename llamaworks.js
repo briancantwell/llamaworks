433662018bbfc302e64fd9c3512e1183d7d92e54
llama-anteater <cantwelb@uci.edu>
2021-05-18 22:47:31 -0700

    final branch

--- llamaworks.js
+++ llamaworks.js
@@ -2,6 +2,10 @@
 
 import {SVG} from './svg.min.js';
 
+/**
+ *
+ * @type {{Button: (function(*=): {move: function(*=, *=): void, onclick: function(*): void, label: function(*=, *=, *=): void}), CheckBox: (function(*=, *=): {move: function(*=, *=): void, label: function(*=, *=, *=): void}), RadioButton: (function(*=, *=): {move: function(*=, *=): void, label: function(*=, *=, *=): void}), TextBox: TextBox, ProgressBar: (function(*=, *=, *=): {move: function(*=, *=): void, setProgress: function(*): void, getIncrement: function(): number, getProgress: function(): number}), ScrollBar: (function(*=, *=): {move: function(*=, *=): void, position: function(): *})}}
+ */
 var LLama = (function() {
     var draw = SVG().addTo('body').size('500', '500');
 
@@ -15,6 +19,12 @@ var LLama = (function() {
         active: '#3d3d3d',
 
     }
+    /**
+     * Creates a button
+     * @param string
+     * @returns {{move: move, onclick: onclick, label: label}}
+     * @constructor
+     */
     var Button = function(string = 'button'){
 
         var group = draw.nested()
@@ -76,8 +86,17 @@ var LLama = (function() {
     }
 
 
-    var CheckBox = function(text = 'radio', qty= 1) {
-
+    /**
+     *
+     * @param text
+     * @param qty
+     * @returns {{move: move, label: label}}
+     * @constructor
+     */
+    var CheckBox = function(text = 'checkbox', qty= 1) {
+        /*TODO:
+            - implement creation of checkbox groups
+         */
         /* create the group
         * - sets the overflow attribute to visible to
         *  prevent element cutoff
@@ -154,8 +173,18 @@ var LLama = (function() {
         }
 
     }
+    /**
+     *
+     * @param text
+     * @param qty
+     * @returns {{move: move, label: label}}
+     * @constructor
+     */
     var RadioButton = function(text = 'checkbox', qty= 1) {
-
+        /*TODO:
+            - implement creation of radio button groups
+            - target functionality: only one radio button in the group can be checked
+         */
         /* create the group
         * - sets the overflow attribute to visible to
         *  prevent element cutoff
@@ -231,17 +260,143 @@ var LLama = (function() {
 
 
     }
-    var TextBox = function() {
+
+    /**
+     *
+     * @param width
+     * @param height
+     * @constructor
+     */
+    var TextBox = function(width = 100, height = 150) {
+        /*TODO: CURRENTLY NOT FUNCTIONAL*/
+        // var group = draw.nested();
+
+        // var backgroundBar = group.rect(width,height).fill(config.primary).radius(5);
+        // var text = group.text("I NEED TACOS!").fill(config.primary)
+        // var caret = text.line(10)
 
 
+        var textBox = draw.rect(width, height)//foreignObject(width, height)
+        // group.add(foreignObject)
+        var text = draw.text('input').build(true)
+
+
+
+        // group.click(function(){
+        //    textBox.focus()
+        // });
+
+        // return {
+        //     move: function (x, y) {
+        //         foreignObject.move(x, y)
+        //     },
+        //     onclick: function (eventHandler) {
+        //         clickEvent = eventHandler
+        //     },
+        //     text: function(){
+        //
+        //     }
+        //
+        // }
     }
-    var ScrollBar = function() {
+
+    /**
+     *
+     * @param height
+     * @param width
+     * @returns {{move: move, position: (function(): *)}}
+     * @constructor
+     */
+    var ScrollBar = function(height = 200, width = 15) {
+        var mouseY = null
+        var group = draw.nested();
+        var indicatorSize = 25;
+        var backgroundBar = group.rect(width,height).fill(config.secondary).radius(5);
+        var indicator = group.rect(width, indicatorSize).fill(config.primary).radius(5);
+        var hold = false
+        var position = indicator.y()
+        var bounds = [backgroundBar.y(), backgroundBar.height()-indicatorSize]
 
 
+
+        document.addEventListener('mousemove', (event) => {
+            mouseY = event.clientY
+        });
+        group.mouseup(function(){
+            hold = false;
+
+        });
+        group.mouseout(function(){
+            hold = false;
+
+        });
+        group.mousemove(function (){
+               if(hold) {
+                   if((mouseY - (indicatorSize / 1.5) >= bounds[1])){
+                       indicator.y(bounds[1])
+                   }
+                   else if((mouseY - (indicatorSize / 1.5) <= bounds[0])){
+                       indicator.y(bounds[0])
+                   }else {
+                       indicator.y((mouseY - (indicatorSize / 1.5)));
+                   }
+               }
+        });
+        group.mousedown(function(){
+            hold = true;
+        })
+        group.click(function(){
+            indicator.animate(100).y((mouseY - (indicatorSize / 1.5)));
+            if((mouseY - (indicatorSize / 1.5) >= bounds[1])){
+                indicator.animate(100).y(bounds[1])
+            }
+            else if((mouseY - (indicatorSize / 1.5) <= bounds[0])){
+                indicator.animate(100).y(bounds[0])
+            }
+        });
+
+        return {
+            move: function (x, y) {
+                foreignObject.move(x, y)
+            },
+            position: function(){
+                return position
+            }
+        }
     }
-    var ProgressBar = function() {
+
+    /**
+     *
+     * @param width
+     * @param height
+     * @param increment
+     * @returns {{move: move, setProgress: setProgress, getIncrement: (function(): number), getProgress: (function(): number)}}
+     * @constructor
+     */
+    var ProgressBar = function(width = 100, height = 15, increment = width/100) {
+        var currentProgress = 0;
+        var group = draw.nested();
+        var backgroundBar = group.rect(width,height).fill(config.secondary);
+        var progressBar = group.rect(currentProgress, height).fill(config.primary);
 
 
+        return {
+            move: function (x, y) {
+                foreignObject.move(x, y)
+            },
+            getIncrement: function(){
+              return increment;
+            },
+            setProgress: function(num){
+                  if(num <= 100 || num >= 0) {
+                      currentProgress = num;
+                      progressBar.animate().width(currentProgress * increment);
+                  }
+            },
+            getProgress: function() {
+                return currentProgress
+            }
+        }
     }
 
     return {Button, CheckBox, RadioButton, TextBox, ScrollBar, ProgressBar }
--- ui.js
+++ ui.js
@@ -1,27 +1,42 @@
 import { LLama } from './llamaworks.js';
 
     // Implement a MyToolkit Button
-    var btn = new LLama.Button;
-    const label = 'tacos'
-    btn.move(80,80);
-    btn.label(label, 20, 'Cooper');
+    // var btn = new LLama.Button;
+    // const label = 'tacos'
+    // btn.move(80,80);
+    // btn.label(label, 20, 'Cooper');
+    //
+    // btn.onclick(function(e){
+    //     console.log(e);
+    //     console.log('button clicked');
+    // });
+    //
+    //
+    // var checkbox = new LLama.CheckBox;
+    // checkbox.move(80, 20);
+    // checkbox.label('toast', 12, 'cooper');
 
-    btn.onclick(function(e){
-        console.log(e);
-        console.log('button clicked');
-    });
-
-
-    var checkbox = new LLama.CheckBox;
-    checkbox.move(80, 20);
-    checkbox.label('toast', 12, 'cooper');
-
-    checkbox.onclick(function(e){
-        console.log(e);
-        console.log('button clicked');
-    });
 
     // var radio = new LLama.RadioButton();
     // radio.move(80, 20);
     // radio.label('toast', 12, 'cooper');
     //
+
+    var textBox = new LLama.TextBox();
+
+    var progressBar = new LLama.ProgressBar(275);
+
+    // var scrollBar = new LLama.ScrollBar(275);
+
+
+
+
+
+
+
+
+
+
+
+
+
